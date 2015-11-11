6a38e8050f83ef8c99b5b52fb663d9096b3399cc
niconan <nick.hax@gmail.com>
2015-11-10 21:26:29 -0800

    build profiles on user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
     authenticated :user do
