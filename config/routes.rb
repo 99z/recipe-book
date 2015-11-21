1b19752ac922cb7837ad9b4bf13ecb3d7708ae7d
niconan <nick.hax@gmail.com>
2015-11-21 09:57:44 -0800

    update home and sign in page styling, add recipe search and sorting

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   scope :api do
     scope :v1 do
-      resources :users, only: [:index, :destroy]
+      resources :users, only: [:index, :destroy, :show]
       resources :recipes, only: [:create, :show, :index, :update, :destroy]
       resources :profiles
     end
